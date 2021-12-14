ba5737afbd74ced34f08c7870a4fa2f56cf11442
Mwambi <collins.mwambi@strathmore.edu>
2021-12-14 19:15:16 +0300

    Student login page

--- studentlogin.php
+++ studentlogin.php
@@ -1,12 +1,41 @@
-<!DOCTYPE html>
-<html>
-<body>
+<!doctype html>
+<html lang="en">
+  
+<head>
+    <meta charset="utf-8">
 
-<h1>My first PHP page</h1>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+     <link href="stulogin.css" rel="stylesheet">
+ 
+    </head>
 
-<?php
-echo "Hello Worlvdsdd!";
-?>
+  <body class="text-center">
+    
+<main class="form-signin">
+  <form>
+    <img class="mb-4" src="logo.jpg" alt="" width="100" height="90">
+    <h1 class="h3 mb-3 fw-normal">Student Login</h1>
 
-</body>
+    <div class="form-floating">
+      <input type="number" class="form-control" id="studentno" placeholder="name@example.com">
+      <label for="studentno">Student No.</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="sPassword" placeholder="Password">
+      <label for="sPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-success" type="submit">Sign in</button>
+    <p class="mt-5 mb-3 ">&copy; Anpet Academy</p>
+  </form>
+</main>
+
+
+    
+  </body>
 </html>
